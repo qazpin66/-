c197fda9c649e7e0908a42f487d74ef2e50a2b08
qazpin66 <17633591701@163.com>
2024-12-04 19:49:30 +0800

    Delete 检测.lua

--- 检测.lua
+++
@@ -1,7 +0,0 @@
-if game.PlaceId == 7239319209 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/qazpin66/-/refs/heads/main/xiaoheehez.lua"))()
-elseif game.PlaceId == 189707 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/qazpin66/-/refs/heads/main/%E5%8A%A0%E5%AF%86%E4%BC%A0%E9%80%81.lua"))()
-else
-loadstring(game:HttpGet("https://raw.githubusercontent.com/qazpin66/-/refs/heads/main/%E9%B9%A41.5.lua"))()
-end
